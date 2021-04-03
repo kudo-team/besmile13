1b1d211b0c700d1803b7d2de092dcba576600984
Takayuki Heike <2beemic@gmail.com>
2021-04-03 14:17:51 +0900

    UXの向上及びクッキー処理の修正

--- BeSmile13ekimae_theme/js/usability.js
+++ BeSmile13ekimae_theme/js/usability.js
@@ -1,28 +1,21 @@
 window.addEventListener('load', function() {
-    setTextSize();
+    getCookiesAndSetUp();
+    alert(document.cookie);
 });
-function setTextSize() {
-    cookieFontSize = '100%';
+function setColor() {
     if(document.cookie != null) {
         var cookies = document.cookie;
         var cookiesArray = cookies.split(';');
-
         for (var c of cookiesArray) {
             var cArray = c.split('=');
-            if (cArray[0] == 'fontSize') {
-                cookieFontSize = cArray[1];  // [key,value]
+            if (cArray[0] == 'color') {
+                colorFile = cArray[1];  // [key,value]
+                changeCSS(colorFile);
             }
         }
-    }
-    if(cookieFontSize != '100%') {
-        currentSize = cookieFontSize;
-        selectSize = cookieFontSize;
     } else {
-        currentSize = document.getElementById("body").style.fontSize;
-        selectSize = '100%';
+        changeCSS('color01.css');
     }
-    document.getElementById("body").style.fontSize = selectSize;
-    setCookie(selectSize);
 }
 function textSizeUp() {
     cookieFontSize = '100%';
@@ -45,14 +38,14 @@ function textSizeUp() {
         selectSize = "";
     }
     if (currentSize == "100%") {
+        document.getElementById("fontZoom").style.opacity = '1';
         selectSize = "110%";
     } else if (currentSize == "110%") {
         selectSize = "120%";
     } else if (currentSize == "120%") {
         selectSize = "130%";
+        document.getElementById("fontZoom").style.opacity = '0.2';
     } else if (currentSize == "130%") {
-        m = escape("これ以上文字のサイズを大きくできません。");
-        alert(unescape(m));
         selectSize = "130%";
     } else {
         selectSize = "110%";
@@ -65,13 +58,38 @@ function textSizeReset() {
     currentSize = document.getElementById("body").style.fontSize;
     currentSize = "";
     document.getElementById("body").style.fontSize = currentSize;
+    document.getElementById("fontZoom").style.opacity = '1';
     setCookie(currentSize);
 }
 
 function changeCSS(cssfile) {
     document.getElementById("css").href = "https://13-sunplace-osaka.com/wp/wp-content/themes/BeSmile13ekimae_theme/css/" + cssfile;
+    document.cookie = "color=" + cssfile + "; expires=" + new Date(2030, 1).toUTCString();
 }
 
 function setCookie(percent) {
-    document.cookie = "fontSize=" + percent;
+    document.cookie = "fontSize=" + percent + "; expires=" + new Date(2030, 1).toUTCString();
+}
+
+function getCookiesAndSetUp() {
+    if(document.cookie != null) {
+        var cookies = document.cookie;
+        var cookiesArray = cookies.split(';');
+
+        for (var c of cookiesArray) {
+            var cArray = c.split('=');
+            if (cArray[0] == 'fontSize') {
+                cookieFontSize = cArray[1];
+                currentSize = cookieFontSize;
+                selectSize = cookieFontSize;
+                document.getElementById("body").style.fontSize = selectSize;
+                if(currentSize == '130%') {
+                    document.getElementById("fontZoom").style.opacity = '0.2';
+                }
+            } else if (cArray[0] == 'color') {
+                cookieColorFile = cArray[1];
+                changeCSS(cookieColorFile);
+            }
+        }
+    }
 }
