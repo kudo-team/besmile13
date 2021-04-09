3f6e9c6e8d6b16a096248873e1dd076603e5358a
Takayuki Heike <2beemic@gmail.com>
2021-04-09 13:45:23 +0900

    .htaccessとか404.phpとか

--- rename.php
+++ rename.php
@@ -1,6 +1,31 @@
 <?php
 
-$base_dir = "./pdf/";
-$new_dir = "../pdf/";
+$file = "index.php";
 
-echo rename($base_dir, $new_dir);
+//$f_r = file($file);
+//echo '<pre>';
+//echo 'here';
+//echo implode($f_r);
+//exit();
+$data = <<<EOF
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
+EOF;
+
+$fp = fopen($file,"w");
+echo fwrite($fp,$data);
